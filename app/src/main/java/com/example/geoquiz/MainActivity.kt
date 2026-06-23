bc8cc2d57ee89b19da32e660bc7dee839c4905e7
composedemo <tomgrim24@gmail.com>
2026-06-24 03:09:57 +0700

    Clean template code

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -6,24 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.geoquiz.ui.theme.GeoQuizTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GeoQuizTheme {
+            MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GeoQuizScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +28,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GeoQuizScreen(modifier: Modifier = Modifier) {
+    // Здесь будет код
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GeoQuizTheme {
-        Greeting("Android")
+fun GeoQuizPreview() {
+    MaterialTheme {
+        GeoQuizScreen()
     }
 }
